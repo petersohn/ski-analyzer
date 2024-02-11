860286116480d3a343ef602b77af73b4ef0a9b11
Peter Szabados <kangirigungi@gmail.com>
2024-02-11 15:22:54 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdout, Write};
+use std::error::Error;
+
+use curl::easy::Easy;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut easy = Easy::new();
+    easy.url("https://www.rust-lang.org/")?;
+    easy.write_function(|data| {
+        stdout().write_all(data).unwrap();
+        Ok(data.len())
+    })?;
+    easy.perform()?;
+
+    println!("{}", easy.response_code()?);
+
+    Ok(())
 }
