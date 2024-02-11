afc8a1186c8f2d41ab84eea4cac120a7e9f05eca
Peter Szabados <kangirigungi@gmail.com>
2024-02-11 17:40:14 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,51 @@
-use std::io::{stdout, Write};
+use std::io::{stdout, Read, Write};
 use std::error::Error;
 
 use curl::easy::Easy;
+use url::form_urlencoded;
+//
+// fn main() -> Result<(), Box<dyn Error>> {
+//     // let input: String = form_urlencoded::byte_serialize("[out:json];rel(3545276);>;out;".as_bytes()).collect();
+//     // let mut input_bytes = input.as_bytes();
+//     let mut asd = "this is the body".as_bytes();
+//
+//     let mut easy = Easy::new();
+//     easy.url("https://overpass-api.de/api/interpreter")?;
+//     easy.post(true)?;
+//     easy.read_function(|buf| {
+//         Ok(asd.read(buf).unwrap_or(0))
+//     })?;
+//     easy.write_function(|data| {
+//         stdout().write_all(data).unwrap();
+//         Ok(data.len())
+//     })?;
+//     easy.perform()?;
+//
+//     println!("{}", easy.response_code()?);
+//
+//     Ok(())
+// }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut input: String = form_urlencoded::byte_serialize("[out:json];rel(3545276);>;out;".as_bytes()).collect();
+    input.insert_str(0, "data=");
+    println!("{}", input);
+    let mut input_bytes = input.as_bytes();
+
     let mut easy = Easy::new();
-    easy.url("https://www.rust-lang.org/")?;
-    easy.write_function(|data| {
+    easy.url("https://overpass-api.de/api/interpreter")?;
+    easy.post(true)?;
+    easy.post_field_size(input_bytes.len() as u64)?;
+
+    let mut transfer = easy.transfer();
+    transfer.read_function(|buf| {
+        Ok(input_bytes.read(buf).unwrap_or(0))
+    })?;
+    transfer.write_function(|data| {
         stdout().write_all(data).unwrap();
         Ok(data.len())
     })?;
-    easy.perform()?;
-
-    println!("{}", easy.response_code()?);
+    transfer.perform()?;
 
     Ok(())
 }
