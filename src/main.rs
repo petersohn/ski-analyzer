fa078b452e0109c2bbb2bcc1893c832f77abcad7
Peter Szabados <kangirigungi@gmail.com>
2024-03-16 20:40:18 +0100

    Rework command line

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use osm_reader::Document;
 use ski_area::SkiArea;
 
 use std::error::Error;
+use std::fs::OpenOptions;
+use std::io::{Read, Write};
 
 mod error;
 mod osm_query;
@@ -11,24 +13,71 @@ mod ski_area;
 
 #[derive(Parser)]
 struct Args {
-    /// Name of the ski area (case insensitive, regex)
-    #[arg(short, long)]
-    name: String,
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Clone, Subcommand)]
+enum Command {
+    /// Query ski area from OSM
+    QueryOsm {
+        /// Name of the ski area (case insensitive, regex)
+        #[arg(short, long)]
+        name: String,
+        /// File name to save result
+        #[arg(short, long)]
+        output: String,
+    },
+    /// Parse ski area from JSON file
+    ParseOsm {
+        /// File name (previously output from QueryOsm)
+        #[arg(short, long)]
+        input: String,
+        /// File name to save result
+        #[arg(short, long)]
+        output: String,
+        /// Pretty print result
+        #[arg(short, long)]
+        pretty: bool,
+    },
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    let json = osm_query::query_ski_area(args.name.as_str())?;
-    let doc = Document::parse(&json)?;
-    eprintln!(
-        "Total nodes: {}, total ways: {}",
-        doc.elements.nodes.len(),
-        doc.elements.ways.len(),
-    );
-    // println!("{:#?}", doc);
-    // serde_json::to_writer(stdout(), &doc)?;
+    match args.command {
+        Command::QueryOsm { name, output } => {
+            let json = osm_query::query_ski_area(name.as_str())?;
+            let mut file =
+                OpenOptions::new().write(true).create(true).open(output)?;
+            file.write(&json)?;
+        }
+        Command::ParseOsm {
+            input,
+            output,
+            pretty,
+        } => {
+            let ski_area = {
+                let mut file = OpenOptions::new().read(true).open(input)?;
+                let mut data = Vec::new();
+                file.read_to_end(&mut data)?;
+                let doc = Document::parse(&data)?;
+                eprintln!(
+                    "Total nodes: {}, total ways: {}",
+                    doc.elements.nodes.len(),
+                    doc.elements.ways.len(),
+                );
+                SkiArea::parse(&doc)?
+            };
 
-    let ski_area = SkiArea::parse(&doc)?;
-    println!("{:#?}", ski_area);
+            let file =
+                OpenOptions::new().write(true).create(true).open(output)?;
+            if pretty {
+                serde_json::to_writer_pretty(file, &ski_area)?;
+            } else {
+                serde_json::to_writer(file, &ski_area)?;
+            }
+        }
+    };
+
     Ok(())
 }
