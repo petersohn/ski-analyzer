b8df21274d3aeeef5e94395fecb87f70370c55b8
Peter Szabados <kangirigungi@gmail.com>
2024-08-25 21:55:16 +0200

    WIP

--- src/gpx_analyzer/use_lift.rs
+++ src/gpx_analyzer/use_lift.rs
@@ -1,4 +1,4 @@
-use super::segments::Segments;
+use super::segments::{Segment, Segments};
 use super::{format_time_option, to_odt};
 use super::{Activity, ActivityType, LiftEnd, UseLift};
 use crate::collection::Avg;
@@ -6,6 +6,7 @@ use crate::config::get_config;
 use crate::ski_area::{Lift, SkiArea};
 
 use std::fmt::Debug;
+use std::mem::take;
 
 use geo::{
     BoundingRect, Closest, HaversineClosestPoint, HaversineDistance,
@@ -69,28 +70,33 @@ enum LiftResult {
     Failure,
 }
 
-struct LiftCandidate<'s, 'g> {
+type SegmentCoordinate = (usize, usize);
+
+struct LiftCandidate<'s> {
     data: UseLift<'s>,
-    route: Segments<'g>,
-    previous_cutoff: (usize, usize),
     result: LiftResult,
     lift_length: f64,
+    possible_begins: Vec<SegmentCoordinate>,
+    possible_ends: Vec<SegmentCoordinate>,
     avg_distance: Avg<f64>,
     distance_from_begin: f64,
     direction_known: bool,
 }
 
-impl<'s, 'g> LiftCandidate<'s, 'g> {
+impl<'s> LiftCandidate<'s> {
     fn new(
         lift: &'s Lift,
-        previous_cutoff: (usize, usize),
-        point: &'g Waypoint,
+        coordinate: SegmentCoordinate,
+        point: &Waypoint,
     ) -> Option<Self> {
         let p = point.point();
-        get_distance_from_begin(lift, &p).map(|distance| {
+        get_distance_from_begin(lift, &p).and_then(|distance| {
             let lift_length = lift.line.item.haversine_length();
             let station = get_station(lift, &p);
-            LiftCandidate {
+            if station.is_none() && coordinate.1 != 0 {
+                return None;
+            }
+            Some(LiftCandidate {
                 data: UseLift {
                     lift,
                     begin_time: point.time.map(|t| t.into()),
@@ -99,34 +105,34 @@ impl<'s, 'g> LiftCandidate<'s, 'g> {
                     end_station: station,
                     is_reverse: false,
                 },
-                route: vec![vec![point]],
-                previous_cutoff,
+                possible_begins: vec![coordinate],
+                possible_ends: vec![],
                 result: LiftResult::NotFinished,
                 lift_length,
                 avg_distance: Avg::new(),
                 distance_from_begin: distance,
                 direction_known: false,
-            }
+            })
         })
     }
 
     fn find<It>(
         it: It,
-        previous_cutoff: (usize, usize),
-        point: &'g Waypoint,
-    ) -> Vec<LiftCandidate<'s, 'g>>
+        coordinate: SegmentCoordinate,
+        point: &Waypoint,
+    ) -> Vec<LiftCandidate<'s>>
     where
         It: Iterator<Item = &'s Lift>,
     {
         it.filter(|l| l.line.bounding_rect().intersects(&point.point()))
-            .filter_map(|l| LiftCandidate::new(l, previous_cutoff, point))
+            .filter_map(|l| LiftCandidate::new(l, coordinate, point))
             .collect()
     }
 
     fn add_point(
         &mut self,
-        point: &'g Waypoint,
-        is_new_segment: bool,
+        point: &Waypoint,
+        coordinate: SegmentCoordinate,
     ) -> LiftResult {
         if self.result != LiftResult::NotFinished {
             return self.result;
@@ -135,7 +141,7 @@ impl<'s, 'g> LiftCandidate<'s, 'g> {
         let distance = match get_distance_from_begin(self.data.lift, &p) {
             Some(d) => d,
             None => {
-                if is_new_segment // We might have lost some data
+                if coordinate.1 == 0 // We might have lost some data
                     || self.data.lift.can_disembark // You fell out of a draglift
                     || self.data.end_station.is_some()
                 {
@@ -158,7 +164,24 @@ impl<'s, 'g> LiftCandidate<'s, 'g> {
             }
         }
         self.avg_distance.add(distance);
-        self.data.end_station = get_station(self.data.lift, &p);
+        let station = get_station(self.data.lift, &p);
+        match station {
+            Some(s) => {
+                if self.data.begin_station == Some(s) {
+                    self.possible_begins.push(coordinate);
+                } else {
+                    self.data.end_station = station;
+                    self.possible_ends.push(coordinate);
+                }
+            }
+            None => {
+                self.data.end_station = None;
+                self.possible_ends.clear();
+                if self.data.lift.can_disembark {
+                    self.possible_ends.push(coordinate);
+                }
+            }
+        }
         self.data.end_time = point.time.map(|t| t.into());
         LiftResult::NotFinished
     }
@@ -167,14 +190,35 @@ impl<'s, 'g> LiftCandidate<'s, 'g> {
         self.result = result;
         result
     }
+
+    fn commit(self) -> UseLift<'s> {
+        self.data
+    }
 }
 
-fn continue_lift<'s>(
-    lift: &mut UseLift<'s>,
-    point: &Waypoint,
-    first_point: bool,
-) -> LiftResult {
-    LiftResult::NotFinished
+fn commit_lift_candidates<'s>(
+    candidates: Vec<LiftCandidate<'s>>,
+) -> Vec<(ActivityType<'s>, SegmentCoordinate)> {
+    vec![]
+}
+
+fn split_route<'g>(
+    route: &mut Segments<'g>,
+    coord: SegmentCoordinate,
+) -> Segments<'g> {
+    if coord.1 == 0 {
+        route.drain(coord.0..).collect()
+    } else {
+        let first_segment: Segment = route[coord.0].drain(coord.1..).collect();
+        if coord.0 == route.len() - 1 {
+            vec![first_segment]
+        } else {
+            [first_segment]
+                .into_iter()
+                .chain(route.drain(coord.0 + 1..))
+                .collect()
+        }
+    }
 }
 
 pub fn find_lift_usage<'s, 'g>(
@@ -183,49 +227,59 @@ pub fn find_lift_usage<'s, 'g>(
 ) -> Vec<Activity<'s, 'g>> {
     let mut result = Vec::new();
 
-    let mut current: Activity<'s, 'g> = Activity::default();
-    let mut starts_with_new_segment = false;
-
-    let config = get_config();
+    type Candidates<'s> = Vec<LiftCandidate<'s>>;
+    let mut current_route: Segments<'g> = Vec::new();
+    let mut candidates: Candidates = Vec::new();
+    let mut finished_candidates: Candidates = Vec::new();
 
     for segment in segments {
-        let mut first_point = true;
+        let segment_id = current_route.len();
+        let mut route_segment: Segment = Vec::new();
         for point in segment {
-            if let ActivityType::UseLift(lift) = &mut current.type_ {
-                match continue_lift(lift, point, first_point) {
-                    LiftResult::NotFinished => (),
-                    LiftResult::Finished => {
-                        result.push(std::mem::take(&mut current))
-                    }
-                    LiftResult::Failure => {
-                        if config.is_vv() {
-                            eprintln!("Failed to parse lift after {} points, begin={} end={}",
-                                current.route.iter().map(|s| s.len()).fold(0, |a, b| a + b),
-                                format_time_option(lift.begin_time),
-                                format_time_option(to_odt(point.time)));
-                        }
-                        current.type_ = ActivityType::Unknown;
-                        if let Some(Activity {
-                            type_: ActivityType::Unknown,
-                            ref mut route,
-                        }) = result.last_mut()
-                        {
-                            if !starts_with_new_segment {
-                                route
-                                    .last_mut()
-                                    .unwrap()
-                                    .append(&mut current.route.remove(0));
-                            }
-                            route.append(&mut current.route);
-                        } else {
-                            result.push(std::mem::take(&mut current));
-                        }
-                    }
+            let coordinate = (segment_id, route_segment.len());
+            let (mut finished, unfinished): (Candidates, Candidates) =
+                candidates
+                    .into_iter()
+                    .filter_map(|mut l| match l.add_point(point, coordinate) {
+                        LiftResult::Failure => None,
+                        _ => Some(l),
+                    })
+                    .partition(|l| l.result == LiftResult::Finished);
+            candidates = unfinished;
+            finished_candidates.append(&mut finished);
+
+            if candidates.is_empty() && !finished_candidates.is_empty() {
+                let mut to_add: Vec<Activity<'s, 'g>> = Vec::new();
+                for (type_, coord) in
+                    commit_lift_candidates(take(&mut finished_candidates))
+                        .into_iter()
+                        .rev()
+                {
+                    let route = split_route(&mut current_route, coord);
+                    to_add.push(Activity { type_, route });
                 }
-            } else {
+                result.reserve(to_add.len());
+                to_add.into_iter().rev().for_each(|r| result.push(r));
             }
-            first_point = false;
+
+            let mut new_candidates = LiftCandidate::find(
+                ski_area.lifts.iter().filter(|l| {
+                    candidates
+                        .iter()
+                        .chain(finished_candidates.iter())
+                        .find(|c| {
+                            (*l as *const Lift) == (c.data.lift as *const Lift)
+                        })
+                        .is_none()
+                }),
+                coordinate,
+                point,
+            );
+            candidates.append(&mut new_candidates);
+
+            route_segment.push(point);
         }
+        current_route.push(route_segment);
     }
 
     result
