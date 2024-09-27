bea3e07cab02fc7ee1fd3b23c6e347c82b980849
Peter Szabados <kangirigungi@gmail.com>
2024-09-27 20:58:09 +0200

    Move Tauri logic to bin

--- src-tauri/src/bin/ski-analyzer.rs
+++ src-tauri/src/bin/ski-analyzer.rs
@@ -1,6 +1,42 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use core::str;
+use std::error::Error;
+use std::fs::OpenOptions;
+use std::io::Read;
+
+fn load_file_inner(path: String) -> Result<String, Box<dyn Error>> {
+    let mut file = OpenOptions::new().read(true).open(path)?;
+    let mut data = Vec::new();
+    file.read_to_end(&mut data)?;
+    Ok(str::from_utf8(&data)?.to_string())
+}
+
+#[tauri::command]
+fn load_file(path: String) -> Result<String, String> {
+    load_file_inner(path).map_err(|e| e.to_string())
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![load_file])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
 fn main() {
-    ski_analyzer::run();
+    run();
 }
