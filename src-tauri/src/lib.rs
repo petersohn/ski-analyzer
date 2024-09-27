19e91235deb7207c6593bfca86c14564c854aa3a
Peter Szabados <kangirigungi@gmail.com>
2024-09-27 21:03:09 +0200

    Revert "Move Tauri logic to bin"
    This reverts commit bea3e07cab02fc7ee1fd3b23c6e347c82b980849.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,9 @@
+use core::str;
+use std::error::Error;
+use std::fs::OpenOptions;
+use std::io::Read;
+use tauri::{Emitter, WindowEvent};
+
 pub mod config;
 pub mod error;
 pub mod gpx_analyzer;
@@ -14,3 +20,34 @@ mod multipolygon_test;
 mod osm_reader_test;
 #[cfg(test)]
 mod test_util;
+
+fn load_file_inner(path: String) -> Result<String, Box<dyn Error>> {
+    let mut file = OpenOptions::new().read(true).open(path)?;
+    let mut data = Vec::new();
+    file.read_to_end(&mut data)?;
+    Ok(str::from_utf8(&data)?.to_string())
+}
+
+#[tauri::command]
+fn load_file(path: String) -> Result<String, String> {
+    load_file_inner(path).map_err(|e| e.to_string())
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![load_file])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
