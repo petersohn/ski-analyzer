1f8b23e1fb960b52192ea643b971085a9858ffc3
Peter Szabados <kangirigungi@gmail.com>
2024-09-29 10:13:36 +0200

    Move map handling to service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import {
   Component,
   ChangeDetectorRef,
   ChangeDetectionStrategy,
-  ViewChild,
 } from "@angular/core";
 import { invoke } from "@tauri-apps/api/core";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { MainMenuComponent } from "./main-menu.component";
 import { MapComponent } from "./map.component";
 import { SkiArea } from "./types/skiArea";
+import { MapService } from "./map.service";
 
 @Component({
   selector: "app-root",
@@ -19,21 +19,21 @@ import { SkiArea } from "./types/skiArea";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  @ViewChild(MapComponent)
-  public map!: MapComponent;
-
   public loading = false;
 
-  constructor(private changeDetector: ChangeDetectorRef) {}
+  constructor(
+    private readonly changeDetector: ChangeDetectorRef,
+    private readonly mapService: MapService,
+  ) {}
 
   public async loadSkiArea(path: string) {
     const data = JSON.parse(await invoke("load_file", { path }));
-    this.map.loadSkiArea(data as SkiArea);
+    this.mapService.loadSkiArea(data as SkiArea);
   }
 
   public async findSkiArea(name: string) {
     const data = await this.doJob(invoke("find_ski_area", { name }));
-    this.map.loadSkiArea(data as SkiArea);
+    this.mapService.loadSkiArea(data as SkiArea);
   }
 
   private async doJob<T>(job: Promise<T>): Promise<T> {
