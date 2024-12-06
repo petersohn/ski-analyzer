797cd11aeaa6706d1d522ded9dd7a170020496e6
Péter Szabados <pszabados@protonmail.com>
2024-12-06 16:09:01 +0100

    Add filtering to ski area selector

--- src/app/components/ski-area-selector.component.ts
+++ src/app/components/ski-area-selector.component.ts
@@ -1,20 +1,39 @@
-import { Component, Signal, HostListener } from "@angular/core";
-import { MatInput } from "@angular/material/input";
+import {
+  Component,
+  Signal,
+  computed,
+  signal,
+  HostListener,
+} from "@angular/core";
 import { MatListModule } from "@angular/material/list";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { SkiAreaMetadata } from "@/types/skiArea";
 import { ActionsService } from "@/services/actions.service";
 import { MapService } from "@/services/map.service";
+import { filterString } from "@/utils/string";
 
 @Component({
   selector: "ski-area-selector",
   templateUrl: "./ski-area-selector.component.html",
   styleUrl: "./ski-area-selector.component.css",
   standalone: true,
-  imports: [MatInput, MatListModule, MatButtonModule],
+  imports: [
+    MatListModule,
+    MatButtonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
 export class SkiAreaSelectorComponent {
-  public skiAreas: Signal<SkiAreaMetadata[]>;
+  private skiAreas: Signal<SkiAreaMetadata[]>;
+  public filter = signal("");
+  public displayedSkiAreas = computed(() =>
+    this.skiAreas().filter((a) => filterString(a.name, this.filter())),
+  );
 
   constructor(
     private readonly actionsService: ActionsService,
