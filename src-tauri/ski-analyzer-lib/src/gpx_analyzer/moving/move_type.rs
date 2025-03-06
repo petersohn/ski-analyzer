ea9798da39f25ca5f89eff767e5d6da3603c5acc
Péter Szabados <pszabados@protonmail.com>
2025-03-06 14:19:51 +0100

    Use DummyCandidate

--- src-tauri/ski-analyzer-lib/src/gpx_analyzer/moving/move_type.rs
+++ src-tauri/ski-analyzer-lib/src/gpx_analyzer/moving/move_type.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 use super::{
-    process::CandidateFactory,
+    process::{Candidate, CandidateFactory},
     simple_candidate::{
         Constraint, ConstraintLimit, ConstraintType, SimpleCandidateFactory,
     },
@@ -16,75 +16,97 @@ pub enum MoveType {
     Traverse,
 }
 
+struct DummyCandidate {}
+
+impl Candidate for DummyCandidate {
+    fn add_line(
+        &mut self,
+        _wp0: &gpx::Waypoint,
+        _wp1: &gpx::Waypoint,
+    ) -> Option<bool> {
+        Some(true)
+    }
+}
+
+struct DummyCandidateFactory {}
+
+impl CandidateFactory for DummyCandidateFactory {
+    fn create_candidate(&self) -> Box<dyn super::process::Candidate> {
+        Box::new(DummyCandidate {})
+    }
+}
+
 pub fn get_move_candidates() -> HashMap<MoveType, Box<dyn CandidateFactory>> {
-    [
-        (
-            MoveType::Ski,
-            SimpleCandidateFactory::new(vec![
-                Constraint::new(
-                    ConstraintType::Speed,
-                    Some(5.0),
-                    None,
-                    ConstraintLimit::Distance,
-                    100.0,
-                ),
-                Constraint::new(
-                    ConstraintType::Inclination,
-                    None,
-                    Some(-0.05),
-                    ConstraintLimit::Distance,
-                    200.0,
-                ),
-            ]),
-        ),
-        (
-            MoveType::Wait,
-            SimpleCandidateFactory::new(vec![Constraint::new(
-                ConstraintType::Speed,
-                None,
-                Some(1.0),
-                ConstraintLimit::Time,
-                20.0,
-            )]),
-        ),
-        (
-            MoveType::Climb,
-            SimpleCandidateFactory::new(vec![
-                Constraint::new(
-                    ConstraintType::Speed,
-                    Some(0.1),
-                    Some(2.0),
-                    ConstraintLimit::Time,
-                    20.0,
-                ),
-                Constraint::new(
-                    ConstraintType::Inclination,
-                    Some(0.05),
-                    None,
-                    ConstraintLimit::Time,
-                    20.0,
-                ),
-            ]),
-        ),
-        (
-            MoveType::Traverse,
-            SimpleCandidateFactory::new(vec![
-                Constraint::new(
-                    ConstraintType::Speed,
-                    Some(0.5),
-                    Some(5.0),
-                    ConstraintLimit::Time,
-                    20.0,
-                ),
-                Constraint::new(
-                    ConstraintType::Inclination,
-                    Some(-0.1),
-                    Some(0.1),
-                    ConstraintLimit::Distance,
-                    100.0,
-                ),
-            ]),
-        ),
-    ]
-    .into()
+    let factory: Box<dyn CandidateFactory> = Box::new(DummyCandidateFactory {});
+    [(MoveType::Ski, factory)]
+        //[
+        //    (
+        //        MoveType::Ski,
+        //        SimpleCandidateFactory::new(vec![
+        //            Constraint::new(
+        //                ConstraintType::Speed,
+        //                Some(5.0),
+        //                None,
+        //                ConstraintLimit::Distance,
+        //                100.0,
+        //            ),
+        //            Constraint::new(
+        //                ConstraintType::Inclination,
+        //                None,
+        //                Some(-0.05),
+        //                ConstraintLimit::Distance,
+        //                200.0,
+        //            ),
+        //        ]),
+        //    ),
+        //    (
+        //        MoveType::Wait,
+        //        SimpleCandidateFactory::new(vec![Constraint::new(
+        //            ConstraintType::Speed,
+        //            None,
+        //            Some(1.0),
+        //            ConstraintLimit::Time,
+        //            20.0,
+        //        )]),
+        //    ),
+        //    (
+        //        MoveType::Climb,
+        //        SimpleCandidateFactory::new(vec![
+        //            Constraint::new(
+        //                ConstraintType::Speed,
+        //                Some(0.1),
+        //                Some(2.0),
+        //                ConstraintLimit::Time,
+        //                20.0,
+        //            ),
+        //            Constraint::new(
+        //                ConstraintType::Inclination,
+        //                Some(0.05),
+        //                None,
+        //                ConstraintLimit::Time,
+        //                20.0,
+        //            ),
+        //        ]),
+        //    ),
+        //    (
+        //        MoveType::Traverse,
+        //        SimpleCandidateFactory::new(vec![
+        //            Constraint::new(
+        //                ConstraintType::Speed,
+        //                Some(0.5),
+        //                Some(5.0),
+        //                ConstraintLimit::Time,
+        //                20.0,
+        //            ),
+        //            Constraint::new(
+        //                ConstraintType::Inclination,
+        //                Some(-0.1),
+        //                Some(0.1),
+        //                ConstraintLimit::Distance,
+        //                100.0,
+        //            ),
+        //        ]),
+        //    ),
+        //]
+        .into()
 }
